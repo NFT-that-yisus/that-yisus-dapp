2b3cc7e8be0953e83532f330aaff898a5904ccf2
Diego Cardona <person.cardonadiego@gmail.com>
2022-01-12 11:53:49 -0500

    RevelationEvent section finished

--- src/sections/indexPage/RevelationEvent.tsx
+++ src/sections/indexPage/RevelationEvent.tsx
@@ -5,7 +5,7 @@ const RevelationEvent = () => {
         <section className="my-32 mx-8 py-32 rounded-lg text-center px-16" style={{ background: "url(/images/RevelationEvent-background.jpg)", backgroundPosition: "center" }}>
             <h1 className="font-heading text-8xl">La llegada del Salvador</h1>
             <h3 className="text-4xl">Revelación NFTs en metaverso Spatial.io (todos pueden entrar).</h3>
-            <p className="pt-16 text-xl">Fecha y hora: Serán anunciados en Club Discord</p>
+            <p className="mt-16 mx-auto bg-white rounded-lg p-2 text-xl w-1/2">Fecha y hora: Serán anunciados en Club Discord</p>
         </section>
     );
 }
