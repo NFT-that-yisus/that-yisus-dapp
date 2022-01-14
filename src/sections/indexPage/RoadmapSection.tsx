7d4508d2364556d2495cd5167ce0e193c1f138bd
Diego Cardona <person.cardonadiego@gmail.com>
2022-01-14 14:08:15 -0500

    Roadmap Section finished

--- src/sections/indexPage/RoadmapSection.tsx
+++ src/sections/indexPage/RoadmapSection.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const RoadmapSection = () => {
     return (
-        <div className="px-4">
-            <h1 className="font-heading text-6xl">Roadmap 2022</h1>
-            <p>- Pre-sale (prioridad/whitelist) minteo (direcciones comunidad discord)</p>
-            <p>- Minteo libre</p>
-            <p>- La llegada del Salvador (revelación NFTs) en evento metaverso Spatial.io </p>
+        <div className="mx-auto py-32 px-4 w-3/4 text-justify">
+            <h1 className="font-heading text-6xl mb-4 text-center">Roadmap 2022</h1>
+            <p>- 18 Enero. Pre-sale (prioridad/whitelist) minteo (direcciones comunidad discord)</p>
+            <p>- 25 Enero. Minteo libre</p>
+            <p>- 01 Febrero. La llegada del Salvador (revelación NFTs) en evento metaverso Spatial.io </p>
 
-            <b>Jack-o 1.000.000 NFTs 🎃</b>
-            <p>- 01 Abril minteo exclusivo comunidad discord</p>
-            <p>- 07 Abril minteo libre</p>
-            <p>- 30 Abril Inicio producción cinematográfica (película)</p>
-            <p>- 31 Octubre evento prívado físico para los holders de That Yisus Exclusive Limited Collection y se transmitirá en directo por Twitch. En este evento se hace la revelación de NFTs Jack-o y además se mostrará una película de halloween donde los holders de Jack-o habrán tenido su participación y cierto protagonismo según la rareza/valor de su NFT. Además del lanzamiento de una marca de ropa amigable y responsable con el medio ambiente.</p>
+            <p className="mt-8 w-max bg-black text-white font-bold">Jack-o 1.000.000 NFTs 🎃</p>
+            <p>- 01 Abril. Minteo exclusivo comunidad discord</p>
+            <p>- 07 Abril. Minteo libre</p>
+            <p>- 30 Abril. Inicio producción cinematográfica (película)</p>
+            <p>- 31 Octubre. Evento prívado físico para los holders de That Yisus Exclusive Limited Collection y se transmitirá en directo por Twitch. En este evento se hace la revelación de NFTs Jack-o y además se mostrará una película de halloween donde los holders de Jack-o habrán tenido su participación y cierto protagonismo según la rareza/valor de su NFT. Además del lanzamiento de una marca de ropa amigable y responsable con el medio ambiente.</p>
             <p>Jack-o</p>
         </div>
     );
