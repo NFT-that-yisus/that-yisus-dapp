52342c0d3bd5f9a8464e0f82dcf37a9d1ea2e821
Diego Cardona <person.cardonadiego@gmail.com>
2022-01-14 19:35:38 -0500

    InOpensea section styles finished

--- src/sections/indexPage/InOpensea.tsx
+++ src/sections/indexPage/InOpensea.tsx
@@ -8,11 +8,11 @@ const InOpensea = () => {
             <div className="h-80 w-80 bg-gray-600" />
 
             <div className="absolute text-center">
-                <h1 className="text-6xl text-white font-heading mix-blend-overlay">
+                <h1 className="text-6xl md:text-8xl text-white font-heading mix-blend-overlay">
                     Ver That Yisus Exclusive Limited Colletion minteados en Opensea
                 </h1>
                 <button className="mx-auto mt-4 py-4 px-8 flex gap-4 bg-gradient-to-r from-blue-400 to-blue-600 text-white items-center hover:from-blue-600 hover:to-blue-800">
-                    <h3 className="text-xl">Colección en Opensea</h3> 
+                    <h3 className="text-lg md:text-xl">Colección en Opensea</h3> 
                     <img className="w-8 h-8" src="/social-icons/opensea-white.svg" />
                 </button>
             </div>
